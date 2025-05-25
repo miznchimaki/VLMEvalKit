7bdacfdbf5d0e885226ff1182359a6acdda74988
kennymckormick <dhd.efz@gmail.com>
2025-05-25 18:52:18 +0800

    [Minor] Update Gemini-2.5-Pro

--- vlmeval/config.py
+++ vlmeval/config.py
@@ -250,18 +250,12 @@ api_models = {
         Gemini, model="gemini-2.0-pro-exp", temperature=0, retry=10
     ),
     "GeminiFlash2-5": partial(
-        Gemini, model="gemini-2.5-flash-preview-04-17", temperature=0, retry=10
+        Gemini, model="gemini-2.5-flash-preview-05-20", temperature=0, retry=10
     ),
     "GeminiPro2-5": partial(
-        Gemini, model="gemini-2.5-pro-preview-03-25", temperature=0, retry=10
-    ),
-    "GeminiPro2-5-Thinking": partial(
-        Gemini, model="gemini-2.5-pro-preview-03-25", temperature=0, retry=10, thinking_budget=24576
-    ),
-    "GeminiPro2-5-0506": partial(
         Gemini, model="gemini-2.5-pro-preview-05-06", temperature=0, retry=10
     ),
-    "GeminiPro2-5-0506-Thinking": partial(
+    "GeminiPro2-5-Thinking": partial(
         Gemini, model="gemini-2.5-pro-preview-05-06", temperature=0, retry=10, thinking_budget=24576
     ),
     
